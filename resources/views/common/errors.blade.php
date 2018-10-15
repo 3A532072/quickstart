febfd562f921542edfb0d31d6dbd055fc851c9fb
3A532072 <dine1138@gmail.com>
2018-10-15 23:26:08 +0800

    seventh

--- resources/views/common/errors.blade.php
+++ resources/views/common/errors.blade.php
@@ -1,10 +1,8 @@
 @if (count($errors) > 0)
-    <!-- Form Error List -->
+    <!-- 表單錯誤清單 -->
     <div class="alert alert-danger">
-        <strong>Whoops! Something went wrong!</strong>
-
+        <strong>哎呀！出了些問題！</strong>
         <br><br>
-
         <ul>
             @foreach ($errors->all() as $error)
                 <li>{{ $error }}</li>
